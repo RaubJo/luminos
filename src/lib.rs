a9f763a0892808bdb297780fec5abd020313b837
Joseph Raub <josephraub98@gmail.com>
2025-11-29 00:31:54 -0600

    CS Fix, add rustfmt, and toolchain files

--- src/lib.rs
+++ src/lib.rs
@@ -1,17 +1,17 @@
 // src/luminos/lib.rs
 
 pub mod support {
-	pub use luminos_support::*;
+    pub use luminos_support::*;
 }
 
 pub mod container {
-	pub use luminos_container::*;
+    pub use luminos_container::*;
 }
 
 pub mod contracts {
-	pub use luminos_contracts::*;
+    pub use luminos_contracts::*;
 }
 
-pub mod config { 
+pub mod config {
     pub use luminos_config::*;
 }
